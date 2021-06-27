0e0792a15bd414dc9896032c87184ab213b85069
Aaron Yu <67659126+aayu3@users.noreply.github.com>
2021-06-27 03:05:49 -0700

    reformated supporter info

--- supporters.js
+++ supporters.js
@@ -23,7 +23,7 @@ allSupportersHTML("#supporters").empty();
 for (var i = 0; i < supporters.length; i ++) {
     var numString = (i+1).toString();
     numString = "0".repeat(4-numString.length) + numString; 
-    allSupportersHTML("#supporters").append("<li><a  target = \"_blank\" href=\"supporters_html/" + numString + ".html\">" + "<img src=\"supporter_images/" + supporters[i].Preview  + ".png\" width = \"5%\" alt=\"" + supporters[i].Name +"\"/>" +  supporters[i].Name + "</a></li>\n");
+    allSupportersHTML("#supporters").append("<li><a  target = \"_blank\" href=\"supporters_html/" + numString + ".html\">" + "<img src=\"supporter_images/" + supporters[i].Icon  + ".png\" width = \"5%\" alt=\"" + supporters[i].Name +"\"/>" +  supporters[i].Name + "</a></li>\n");
     let supporterHTML = SupporterJSONtoHTML(supporters[i]).toString();
     fs.writeFile("./supporters_html/" + numString + ".html", supporterHTML, (error) => { console.log("Unable to add " + numString +".html")});
 }
@@ -37,12 +37,12 @@ function SupporterJSONtoHTML(supporter) {
     templateHTML("#Name").text("No. " + supporter.Number + ": " + supporter.Name);
     templateHTML("#metaTitle").attr("content",supporter.Name);
     templateHTML("#metaDescription").attr("content","Wiki page for " + supporter.Name + ".");
-    templateHTML("#metaImage").attr("content","https://aayu3.github.io/ATBotJSONDependencies/supporter_images/" + supporter.Preview + ".png");
+    templateHTML("#metaImage").attr("content","https://aayu3.github.io/ATBotJSONDependencies/supporter_images/" + supporter.Icon + ".png");
     templateHTML("#ogTitle").attr("content",supporter.Name);
     templateHTML("#ogDescription").attr("content","Wiki page for " + supporter.Name + ".");
-    templateHTML("#ogImage").attr("content","https://aayu3.github.io/ATBotJSONDependencies/supporter_images/" + supporter.Preview + ".png");
-    templateHTML("#ogImage").attr("content","https://aayu3.github.io/ATBotJSONDependencies/supporter_images/" + supporter.Preview + ".png");
-    templateHTML("#faviconInfo").attr("href", "../supporter_images/" + supporter.Preview + ".png");
+    templateHTML("#ogImage").attr("content","https://aayu3.github.io/ATBotJSONDependencies/supporter_images/" + supporter.Icon + ".png");
+    templateHTML("#ogImage").attr("content","https://aayu3.github.io/ATBotJSONDependencies/supporter_images/" + supporter.Icon + ".png");
+    templateHTML("#faviconInfo").attr("href", "../supporter_images/" + supporter.Icon + ".png");
     // Set type
     if (supporter.Type === "Protect") {
         templateHTML("#Type").attr("src","../wiki_icons/protect.png");
@@ -53,7 +53,7 @@ function SupporterJSONtoHTML(supporter) {
     }
 
     // Set rarity
-    if (supporter.Rarity === "R") {
+    if (supporter.Rarity === "Rare") {
         templateHTML("#Rarity").attr("src","../wiki_icons/rare.png");
     } else if (supporter.Rarity === "SR") {
         templateHTML("#Rarity").attr("src","../wiki_icons/sr.png");
@@ -61,46 +61,45 @@ function SupporterJSONtoHTML(supporter) {
         templateHTML("#Rarity").attr("src","../wiki_icons/ur.png");
     }
     templateHTML("#Source").append(supporter.Source);
-    templateHTML("#Status").append(supporter.Status);
+
     // Set Unawakened, Awakened, and starting Video
-    templateHTML("#UnawakenedButton").attr("onclick","document.getElementById('SupporterImage').src='../supporter_images/" + supporter.Unawakened + ".png'");
-    templateHTML("#AwakenedButton").attr("onclick","document.getElementById('SupporterImage').src='../supporter_images/" + supporter.Awakened + ".png'");
-    templateHTML("#SupporterImage").attr("src","../supporter_images/" + supporter.Unawakened + ".png");
+    templateHTML("#UnawakenedButton").attr("onclick","document.getElementById('SupporterImage').src='../supporter_images/" + supporter.Icon + "_0.png'");
+    templateHTML("#AwakenedButton").attr("onclick","document.getElementById('SupporterImage').src='../supporter_images/" + supporter.Icon + "_1.png'");
+    templateHTML("#SupporterImage").attr("src","../supporter_images/" + supporter.Icon + "_0.png");
     if (supporter.Rarity ==="R") {
         
     } else {
-        templateHTML("#IntimacyImage").attr("src","../intimacy_images/Supporter_" + supporter.Unawakened + ".png");
+        templateHTML("#IntimacyImage").attr("src","../intimacy_images/Supporter_" + supporter.Icon + "_0.png");
     }
     
     // Subskill Formatting
-    if (supporter.SubSkillName) {
+    if (supporter.SkillName) {
         templateHTML("#SubSkill").text("Sub Skill: ");
-        templateHTML("#SubSkill").append("<u id = 'SubSkillName'>" + supporter.SubSkillName + "</u>");
-        templateHTML("#SubSkillName").text(supporter.SubSkillName);
-        templateHTML("#SubSkillText").text(supporter.SubSkill);
+        templateHTML("#SubSkill").append("<u id = 'SubSkillName'>" + supporter.SkillName + "</u>");
+        templateHTML("#SubSkillName").text(supporter.SkillName);
+        var formatDesc = supporter.SkillDescription;
+        formatDesc = formatDesc.split('[FFFF00FF]').join('<span style="color:#FFFF00FF";>');
+        formatDesc = formatDesc.split('[-]').join('</span>');
+        formatDesc = formatDesc.split('[6BEB00FF]').join('<span style="color:#6BEB00FF";>');
+        formatDesc = formatDesc.split('[FFA500FF]').join('<span style="color:#FFA500FF";>');
+        formatDesc = formatDesc.split('[FF5A00FF]').join('<span style="color:#FF5A00FF";>');
+        templateHTML("#SubSkillText").append(formatDesc);
     }
 
     // Check for Main Skill then format it
     if (supporter.MainSkillName) {
         templateHTML("#LeadSkill").text("Lead Skill: ");
         templateHTML("#LeadSkill").append("<u id = 'LeadSkillName'>" + supporter.MainSkillName +  "</u>");
-        if (supporter.CD && supporter.MainSkillPassive) {
-            templateHTML("#LeadSkill").append(" CD: " + supporter.CD + "s");
-            templateHTML("#CD").text("CD:" + supporter.CD + "s");
-            templateHTML("#ActiveSkill").text("Active Skill: ");
-            templateHTML("#ActiveSkillText").text(supporter.MainSkillActive);
-            templateHTML("#PassiveSkill").text("Passive Skill: ");
-            templateHTML("#PassiveSkillText").text(supporter.MainSkillPassive);
-        } else {
-            if (supporter.CD) {
-                templateHTML("#LeadSkill").append(" CD: " + supporter.CD + "s");
-                templateHTML("#CD").text("CD:" + supporter.CD + "s");
-                templateHTML("#ActiveSkillText").text(supporter.MainSkillActive);
-            }
-            if (supporter.MainSkillPassive) {
-                templateHTML("#PassiveSkillText").text(supporter.MainSkillPassive);
-            }
-        }
+        templateHTML("#LeadSkill").text(supporter.MainSkillName);
+        var formatDesc = supporter.MainSkillDescription;
+        formatDesc = formatDesc.split('[FFFF00FF]').join('<span style="color:#FFFF00FF";>');
+        formatDesc = formatDesc.split('[-]').join('</span>');
+        formatDesc = formatDesc.split('[6BEB00FF]').join('<span style="color:#6BEB00FF";>');
+        formatDesc = formatDesc.split('[FFA500FF]').join('<span style="color:#FFA500FF";>');
+        formatDesc = formatDesc.split('[FF5A00FF]').join('<span style="color:#FF5A00FF";>');
+        templateHTML("#MainSkillText").append(formatDesc);
+        templateHTML("#CD").text("CD:" + supporter.CD + "s");
+        templateHTML("#CD").text("CD:" + supporter.CD + "s");
         
     }
 
